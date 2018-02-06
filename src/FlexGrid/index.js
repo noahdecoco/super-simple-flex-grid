2f87fdad93692b06770f51137328c601c73196ef
Noah Fernandes <>
2018-02-06 10:45:27 +0100

    removed unnecessary data-types

--- src/FlexGrid/index.js
+++ src/FlexGrid/index.js
@@ -7,9 +7,7 @@ const HALF_GUTTER = GUTTER / 2;
 
 const getColumsInPercent = value => `${value / 12 * 100}%`;
 
-export const Column = styled.div.attrs({
-  'data-type': 'column',
-})`
+export const Column = styled.div`
   position: relative;
   flex: ${({ flex }) => (flex ? `0 0 ${getColumsInPercent(flex)}` : 1)};
   padding: ${() => `${HALF_GUTTER}px ${HALF_GUTTER}px ${HALF_GUTTER}px`};
@@ -17,31 +15,27 @@ export const Column = styled.div.attrs({
   margin-left: ${({ push }) => (push ? getColumsInPercent(push) : 0)};
 `;
 
-export const Row = styled.div.attrs({
-  'data-type': 'row',
-})`
+export const Row = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: ${() => `0 ${-HALF_GUTTER}px 0`};
   align-items: ${({ flexAlign }) => (flexAlign ? flexAlign : 'auto')};
 `;
 
-export const Container = styled.div.attrs({
-  'data-type': 'container',
-})`
+export const Container = styled.div`
   margin: auto;
   overflow: hidden;
   max-width: ${() => `${MAX_WIDTH}px`};
 
-  > ${Row}:first-child:not([data-type="container"]) {
-    margin: ${() => `${-HALF_GUTTER}px ${-HALF_GUTTER}px 0`};
-  }
-
-  > ${Row}:last-child:not([data-type="container"]) {
-    margin: ${() => `0 ${-HALF_GUTTER}px ${-HALF_GUTTER}px`};
-  }
-
-  > ${Row}:only-child:not([data-type="container"]) {
-    margin: ${() => `${-HALF_GUTTER}px`};
+  > ${Row} {
+    &:first-child {
+      margin: ${() => `${-HALF_GUTTER}px ${-HALF_GUTTER}px 0`};
+    }
+    &:last-child {
+      margin: ${() => `0 ${-HALF_GUTTER}px ${-HALF_GUTTER}px`};
+    }
+    &:only-child {
+      margin: ${() => `${-HALF_GUTTER}px`};
+    }
   }
 `;
